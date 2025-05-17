074952bdf7c864755d652e937fe12c80cd16a14d
soyyyyy <da5868@naver.com>
2025-05-17 15:44:15 +0900

    feat: 6주차 실습2

--- week6/src/App.tsx
+++ week6/src/App.tsx
@@ -1,57 +1,64 @@
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import axios from 'axios';
 import { useState } from 'react';
 
-const getUsers = async () => {
-  const response = await axios.get('https://api.atsopt-seminar4.site/api/v1/users');
+const userId = 170;
+
+const getMyNickname = async (userId: number) => {
+  const response = await axios.get('https://api.atsopt-seminar4.site/api/v1/users/me', {
+    headers: {
+      userId: userId
+    }
+  });
   return response.data.data;
 };
 
+const patchMyNickname = async (newNickname: string) => {
+  const response = await axios.patch('https://api.atsopt-seminar4.site/api/v1/users', 
+    { nickname: newNickname },
+    {
+      headers: {
+        userId: userId
+      }
+    }
+  );
+  return response.data.data;
+};
 
 function App() {
-  const [isClicked, setIsClicked] = useState(false);
+  const [text, setText] = useState("");
+  const queryClient = useQueryClient();
 
-  const { data } = useQuery({
-    queryKey: ['users'],
-    queryFn: getUsers,
-    enabled: isClicked,
-  })
+  const { data, isLoading } = useQuery({
+    queryKey: ['myNickname', userId],
+    queryFn: () => getMyNickname(userId)
+  });
 
-  // const handleClick = () => {
-  //   setIsClicked((prev) => !prev);
-  // }
+  const { mutate } = useMutation({
+    mutationFn: patchMyNickname,
+    onSuccess: () => {
+      queryClient.invalidateQueries({queryKey: ['myNickname', userId]});
+      setText('');
+    }
+  });
+
+  if (isLoading) return <div>로딩 중...</div>;
 
   return (
-    <>
-      <h1>유저 목록</h1>
-      <button onClick={() => setIsClicked(true)}>데이터 가져오기</button>
-        <ul>
-          {data?.nicknameList.map((nickname: string, idx: number) => (
-            <li key={idx}>{nickname}</li>
-          ))}
-        </ul>
-    </>
-  )
+    <div>
+      내 정보:
+      {data && <span> {data.nickname}</span>}
+      <br />
+      <input
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        placeholder='수정할 닉네임 입력'
+      />
+      <button onClick={() => mutate(text)}>
+        닉네임 수정하기
+      </button>
+    </div>
+  );
 }
 
-export default App
-
-
-
-/*
-  return (
-    <>
-      <h1>유저 목록</h1>
-      <button onClick={handleClick}>데이터 가져오기</button>
-      {isClicked && (
-        <ul>
-          {data?.nicknameList.map((nickname: string, idx: number) => (
-            <li key={idx}>{nickname}</li>
-          ))}
-        </ul>
-      )}
-    </>
-  )
-}
-
-*/
+export default App;
